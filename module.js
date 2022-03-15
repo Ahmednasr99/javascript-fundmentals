97a6070d2db31e9f25ce10194d2b827eacd5df0d
Ahmed Nasr <eng_mimo98@yahoo.com>
2022-03-15 15:54:28 -0400

    add variables

---
+++ module.js
@@ -0,0 +1,10 @@
+let myName = prompt("enter ur name ")
+let month = prompt("enter ur name ")
+let food = prompt("enter ur name ")
+
+
+console.log('my name is ',myName)
+console.log('i was born in ',month)
+console.log('my fav food ',food)
+
+
