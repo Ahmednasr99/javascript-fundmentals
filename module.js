640bb75568ae9b795f1a59722e668efa5f0bd70a
Ahmed Nasr <eng_mimo98@yahoo.com>
2022-03-16 12:11:05 -0400

    work with functions

--- module.js
+++ module.js
@@ -1,10 +1,71 @@
-let myName = prompt("enter ur name ")
-let month = prompt("enter ur name ")
-let food = prompt("enter ur name ")
+/*function quiz(){
+    
+    let myName = prompt("enter ur name ")
+    let month = prompt("enter ur name ")
+    let food = prompt("enter ur name ")
 
 
-console.log('my name is ',myName)
-console.log('i was born in ',month)
-console.log('my fav food ',food)
+    console.log('my name is ',myName)
+    console.log('i was born in ',month)
+    console.log('my fav food ',food)
+
+}
+*/
+
+let  quiz=()=>{
+    
+    let myName = prompt("enter ur name ")
+    let month = prompt("enter ur name ")
+    let food = prompt("enter ur name ")
 
 
+    console.log('my name is ',myName)
+    console.log('i was born in ',month)
+    console.log('my fav food ',food)
+
+}
+
+const notifyA = () =>{
+
+}
+const notifyB = () =>{
+
+}
+
+const print = () =>{
+    console.log("i love to print text");
+}
+
+
+const functionA = () =>{
+    console.log("FUNCTION A BEEN CALELD")
+    functionB()
+
+}
+
+
+const functionB = () =>{
+    console.log("FUNCTION B BEEN CALELD")
+
+}
+
+const functionC = (f) =>{
+    console.log("FUNCTION C BEEN CALELD")
+    f()
+
+}
+
+const great = (person) =>{
+    console.log("hello", person)
+}
+
+
+const blender = (string) =>{
+    return string.split('')
+}
+
+const square = (num, message) =>{
+   console.log(num*num) 
+   console.log("message", message)
+
+}
