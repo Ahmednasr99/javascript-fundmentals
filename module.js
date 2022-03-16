57e882257f63d506fe02afb6293c6eb42157bc08
Ahmed Nasr <eng_mimo98@yahoo.com>
2022-03-16 13:18:19 -0400

    work with functions2

--- module.js
+++ module.js
@@ -1,22 +1,10 @@
-/*function quiz(){
-    
-    let myName = prompt("enter ur name ")
-    let month = prompt("enter ur name ")
-    let food = prompt("enter ur name ")
 
-
-    console.log('my name is ',myName)
-    console.log('i was born in ',month)
-    console.log('my fav food ',food)
-
-}
-*/
 
 let  quiz=()=>{
     
-    let myName = prompt("enter ur name ")
-    let month = prompt("enter ur name ")
-    let food = prompt("enter ur name ")
+    let myName = prompt("enter ur name : ")
+    let month = prompt("birth date : ")
+    let food = prompt("favourite food : ")
 
 
     console.log('my name is ',myName)
