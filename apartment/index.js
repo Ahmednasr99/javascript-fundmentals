62988ea3f93786b7c4196c079db22e304300dc2a
Ahmed Nasr <eng_mimo98@yahoo.com>
2022-03-17 12:03:04 -0400

    apartment details

---
+++ apartment/index.js
@@ -0,0 +1,11 @@
+let apt1 = {bedroom : 4 ,sqft :50 , window : 4}
+let apt2 = {bedroom : 5 ,sqft :80 , window : 8}
+let apt3 = {bedroom : 4 ,sqft :70 , window : 7}
+
+let building = {
+    streetaddress :"41 union ave",
+    laundary : true,
+    allowspets : true,
+    apartments: [apt1,apt2,apt3],
+    
+}
