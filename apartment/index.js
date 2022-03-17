503f7e43fbe128a6705f9b8a9d611c19214dc843
Ahmed Nasr <eng_mimo98@yahoo.com>
2022-03-17 13:27:47 -0400

    apartment details more

--- apartment/index.js
+++ apartment/index.js
@@ -1,11 +1,18 @@
-let apt1 = {bedroom : 4 ,sqft :50 , window : 4}
-let apt2 = {bedroom : 5 ,sqft :80 , window : 8}
-let apt3 = {bedroom : 4 ,sqft :70 , window : 7}
+let tent1 = {name:'john', cscore:400, age:50, salary:15244}
+let tent2 = {name:'osc',  cscore:500,  age:40, salary:15244}
+let tent3 = {name:'joe',  cscore:700,  age:30, salary:15244}
+
+let apt1 = {bedroom : 4 ,sqft :50 , window : 4 ,tents:[]}
+let apt2 = {bedroom : 5 ,sqft :80 , window : 8 ,tents:[]}
+let apt3 = {bedroom : 4 ,sqft :70 , window : 7 ,tents:[]}
+
+
 
 let building = {
     streetaddress :"41 union ave",
     laundary : true,
     allowspets : true,
     apartments: [apt1,apt2,apt3],
+    
     
 }
