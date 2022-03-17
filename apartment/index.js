71b21628a9d950937fa336f63df1eb8edee2b0b1
Ahmed Nasr <eng_mimo98@yahoo.com>
2022-03-17 14:11:25 -0400

    apartment details with tent

--- apartment/index.js
+++ apartment/index.js
@@ -2,9 +2,30 @@ let tent1 = {name:'john', cscore:400, age:50, salary:15244}
 let tent2 = {name:'osc',  cscore:500,  age:40, salary:15244}
 let tent3 = {name:'joe',  cscore:700,  age:30, salary:15244}
 
-let apt1 = {bedroom : 4 ,sqft :50 , window : 4 ,tents:[]}
-let apt2 = {bedroom : 5 ,sqft :80 , window : 8 ,tents:[]}
-let apt3 = {bedroom : 4 ,sqft :70 , window : 7 ,tents:[]}
+let apt1 = {bedroom : 4 ,sqft :50 , window : 4 ,unit : "1A",lease : function(tent){
+    if(this.tents.length===this.bedroom){
+        return `${this.unit} is already full`
+    }else
+    this.tents.push(tent)
+    console.log(tent.name,"has rent out",this.unit)
+
+} ,tents:[]}
+let apt2 = {bedroom : 5 ,sqft :80 , window : 8 ,unit : "2A" ,lease : function(tent){
+    if(this.tents.length===this.bedroom){
+        return `${this.unit} is already full`
+    }else
+    this.tents.push(tent)
+    console.log(tent.name,"has rent out",this.unit)
+
+} ,tents:[]}
+let apt3 = {bedroom : 4 ,sqft :70 , window : 7 ,unit : "3A" ,lease : function(tent){
+    if(this.tents.length===this.bedroom){
+        return `${this.unit} is already full`
+    }else
+    this.tents.push(tent)
+    console.log(tent.name,"has rent out",this.unit)
+
+} ,tents:[]}
 
 
 
